ecc7186ee7712c93936200a01c49d2b25f0883a9
pogopl <79012352+pogopl@users.noreply.github.com>
2024-05-19 14:00:41 +0200

    restoring project folder

--- README.md
+++ README.md
@@ -1,31 +1,34 @@
-1. Aplikacja do zarządzania projektami
-Modele:
+## Aplikacja do zarządzania projektami
+-----
+## Modele:
 
-Project (Projekt)
-Nazwa
-Opis
-Data rozpoczęcia
-Data zakończenia
-Task (Zadanie)
-Nazwa
-Opis
-Data rozpoczęcia
-Data zakończenia
-Status
-Powiązany projekt (Project)
-User (Użytkownik)
-Imię
-Nazwisko
-Email
-Zadania (Tasks)
-Powiązania:
+# 1. Project (Projekt)
+- Nazwa
+- Opis
+- Data rozpoczęcia
+- Data zakończenia
+
+# 2. Task (Zadanie)
+- Nazwa
+- Opis
+- Data rozpoczęcia
+- Data zakończenia
+- Status
+- Powiązany projekt (Project)
+
+# 3. User (Użytkownik)
+- Imię
+- Nazwisko
+- Email
+- Zadania (Tasks)
+
+# Powiązania:
 
 Projekt ma wiele zadań.
 Zadanie należy do jednego projektu.
 Użytkownik może być przypisany do wielu zadań.
-Funkcje:
+
+# Funkcje:
 
 Użytkownicy mogą tworzyć, edytować i usuwać projekty oraz zadania.
 Użytkownicy mogą przypisywać zadania do projektów i użytkowników.
-
-
---
+++ project/www/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from enum import Enum
+
+class Project(models.Model):
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    end_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskStatus(Enum):
+    NOT_STARTED = 'Not Started'
+    IN_PROGRESS = 'In Progress'
+    DONE = 'Done'
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name.replace("_", " ").title()) for key in cls]
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    end_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    status = models.charField(
+            max_length=50,
+            choices=TaskStatus.choices(),
+            default=TaskStatus.NOT_STARTED.value)
+
+    projects = models.ManyToManyField(Project)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    
+    tasks = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return self.last_name + " " + self.name
