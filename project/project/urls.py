0be6dc9fbc8c38eae7ddd4ddcff8f940bb7b994c
pogopl <79012352+pogopl@users.noreply.github.com>
2024-05-19 14:14:09 +0200

    adding urls to urls1

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', views.index, name='index'),
+    path('project/<int:pk>/', views.detail_project, name='detail-project'),
+    path('task/<int:pk>/', views.detail_task, name='detail-task'),
+    path('user/<int:pk>/', views.detail_user, name='detail-user'),
+    path('bootstrap/', views.bootstrap, name='bootstrap'),
 ]
