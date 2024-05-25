32afa61fbf5985654def6e3b3bee46c4b3e66593
pogopl <79012352+pogopl@users.noreply.github.com>
2024-05-25 19:46:58 +0200

    3 step forms

--- project/www/urls.py
+++ project/www/urls.py
@@ -8,3 +8,11 @@ urlpatterns = [
     path('user/<int:pk>/', views.detail_user, name='detail-user'),
     path('bootstrap/', views.bootstrap, name='bootstrap'),
 ]
+
+path('Projrcts/', views.Projects_list, name='Projects-list'),
+path('Tasks/', views.Tasks_list, name='Tasks-list'),
+path('Users/', views.Users_list, name='Users-list'),
+path('add-Project/', views.add_Project, name='add_Project'),
+path('add-Task/', views.add_Task, name='add-Task'),
+path('add-User/', views.add_User, name='add-User'),
+path('saved/', views.saved, name='saved'),
